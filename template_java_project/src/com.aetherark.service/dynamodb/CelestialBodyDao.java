3e80a8df4c8e18586f5a2c546ad8c78988e11f4f
Adam Schraedel <93215502+aschrae6@users.noreply.github.com>
2022-03-28 20:59:35 -0600

    CreateCelestialBody (#19)
    * CreateCelestialBody
    
    * CreateCelestialBody modifications
    
    * CreateCelestialBodyActivity and added Dao methods

--- template_java_project/src/com.aetherark.service/dynamodb/CelestialBodyDao.java
+++ template_java_project/src/com.aetherark.service/dynamodb/CelestialBodyDao.java
@@ -1,11 +1,13 @@
 package com.aetherark.service.dynamodb;
 
 import com.aetherark.service.dynamodb.models.CelestialBody;
+import com.aetherark.service.dynamodb.models.SolarSystem;
 import com.aetherark.service.exceptions.CelestialBodyNotFoundException;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 public class CelestialBodyDao {
@@ -33,7 +35,6 @@ public class CelestialBodyDao {
         if (body == null) {
             throw new CelestialBodyNotFoundException("Could not find planet with id " + id);
         }
-
         return body;
     }
 
@@ -46,5 +47,55 @@ public class CelestialBodyDao {
     public CelestialBody saveCelestialBody(CelestialBody body) {
         dynamoDbMapper.save(body);
         return body;
+    }
+
+    /**
+     * Deletes the {@link CelestialBody} from the DynamoDB table.
+     *
+     * @param body the Celestial Body to be deleted
+     * @return the deleted Celestial Body
+     */
+    public CelestialBody deleteCelestialBody(CelestialBody body) {
+        this.dynamoDbMapper.delete(body);
+        return body;
+    }
+
+    /**
+     * Adds the provided {@link SolarSystem} to the list of SolarSystems of which this {@link CelestialBody} is a member.
+     *
+     * @param bodyId the ID of the Celestial Body which has been added to the Solar System
+     * @param solarSystem the Solar System that now contains the Celestial Body
+     * @return the updated Celestial Body object
+     */
+    public CelestialBody addToBodyMemberSolarSystems(String bodyId, SolarSystem solarSystem) {
+        CelestialBody body = getCelestialBody(bodyId);
+        List<SolarSystem> solarSystemList = body.getMemberSolarSystems();
+        solarSystemList.add(solarSystem);
+        body.setMemberSolarSystems(solarSystemList);
+        return saveCelestialBody(body);
+    }
+
+    /**
+     * Removes the provided {@link SolarSystem} from the list of SolarSystems of which this {@link CelestialBody} is a member.
+     *
+     * @param bodyId the ID of the Celestial Body which has been removed from the Solar System
+     * @param solarSystem the Solar System that no longer contains the Celestial Body
+     * @return the updated Celestial Body object
+     */
+    public CelestialBody removeFromBodyMemberSolarSystems(String bodyId, SolarSystem solarSystem) {
+        CelestialBody body = getCelestialBody(bodyId);
+        List<SolarSystem> solarSystemList = body.getMemberSolarSystems();
+        solarSystemList.remove(solarSystem);
+        body.setMemberSolarSystems(solarSystemList);
+        return saveCelestialBody(body);
+    }
+
+    /**
+     * Deletes every {@link CelestialBody} with a provided id.
+     *
+     * @param bodyIds the List of ids to be deleted
+     */
+    public void deleteCelestialBodiesList(List<String> bodyIds) {
+        dynamoDbMapper.batchDelete(bodyIds);
     }
 }
